877c0677fea5cc34a9279ed52ab6cc473f9f6537
Saimanikanta540 <saipasumarthi540@gmail.com>
2026-06-03 17:21:57 +0530

    created encode and decode methods in simpletokenizerv1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from simpleTokenizerV1 import simpleTokenizerV1
+from SimpleTokenizerV1 import SimpleTokenizerV1
 with open("Data/verdict.txt","r",encoding="utf-8") as file:
     content=file.read()
 
@@ -11,4 +11,14 @@ vocabulary_size=len(all_words)
 
 vocabulary={token:integer for integer,token in enumerate(all_words) }
 
-v1=simpleTokenizerV1(vocabulary)
+v1=SimpleTokenizerV1(vocabulary)
+
+text = """I had always thought Jack Gisburn rather a cheap genius--though a
+good fellow enough--so it was no great surprise to me to hear that,"""
+
+ids=v1.encode(text)
+print(ids)
+print(v1.decode(ids))
+
+
+
--- simpleTokenizerV1.py
+++ simpleTokenizerV1.py
@@ -1,5 +1,18 @@
+import re
+
 class SimpleTokenizerV1:
     def __init__(self,vocabulary):
         self.encoderVocabulary=vocabulary
         self.decoderVocabulary={tokenid:token for token,tokenid in vocabulary.items()}
-    
+    def encode(self,text):
+        preprocessed_input_text = re.split(r'([,.:;?!_()"\']|--|\s)',text)
+        preprocessed_input_text = [
+            item.strip() for item in preprocessed_input_text if item.strip()
+        ]
+        input_text_ids=[self.encoderVocabulary[token] for token in preprocessed_input_text]
+        return input_text_ids
+    def decode(self,input_text_ids):
+        result_text=" ".join(self.decoderVocabulary[tokenid] for tokenid in input_text_ids)
+        # Replace white spaces before specified symbols mentioned below
+        result_text=re.sub(r'"\s+([,.:;?!_()"\'])',r'\1',result_text)    
+        return result_text
