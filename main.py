6fb86efd060e45596a1e39a0b370425981d5a879
Saimanikanta540 <saipasumarthi540@gmail.com>
2026-06-13 17:16:00 +0530

    created input-target pairs

--- main.py
+++ main.py
@@ -13,33 +13,33 @@ all_words=sorted(set(preprocessed))
 vocabulary_size=len(all_words)
 vocabulary={token:integer for integer,token in enumerate(all_words) }
 
-#v1 tokenizer
-v1=SimpleTokenizerV1(vocabulary)
+# #v1 tokenizer
+# v1=SimpleTokenizerV1(vocabulary)
 
-text = """I had always thought Jack Gisburn rather a cheap genius--though a
-good fellow enough--so it was no great surprise to me to hear that,"""
+# text = """I had always thought Jack Gisburn rather a cheap genius--though a
+# good fellow enough--so it was no great surprise to me to hear that,"""
 
-ids=v1.encode(text)
+# ids=v1.encode(text)
 
-print(ids)
-print(v1.decode(ids))
+# print(ids)
+# print(v1.decode(ids))
 
-#Adding Special Context Tokens
-all_words=sorted(set(preprocessed))
-all_words.extend(["<|endoftext|>","<|unk|>"])
-vocabulary={token:integer for integer,token in enumerate(all_words) }
+# #Adding Special Context Tokens
+# all_words=sorted(set(preprocessed))
+# all_words.extend(["<|endoftext|>","<|unk|>"])
+# vocabulary={token:integer for integer,token in enumerate(all_words) }
 
-#v2 tokenizer
-v2=SimpleTokenizerV2(vocabulary)
+# #v2 tokenizer
+# v2=SimpleTokenizerV2(vocabulary)
 
-text1="Hello, do you like tea?"
-text2="In the sunlit teerances of the place"
-concattext=" <|endoftext|> ".join((text1,text2))
+# text1="Hello, do you like tea?"
+# text2="In the sunlit teerances of the place"
+# concattext=" <|endoftext|> ".join((text1,text2))
 
-ids2=v2.encode(concattext)
+# ids2=v2.encode(concattext)
 
-print(ids2)
-print(v2.decode(ids2))
+# print(ids2)
+# print(v2.decode(ids2))
 
 #BPE Tokenizer
 bpe_text="Hi this is a radom text. <|endoftext|> this is a random word dksaimanikantakjdafk."
@@ -48,3 +48,4 @@ bpe_tokenids=bpe_tokenizer.encoder(bpe_text)
 print("BPE Encoding")
 print(bpe_tokenids[:20])
 print(bpe_tokenizer.decoder(bpe_tokenids))
+
